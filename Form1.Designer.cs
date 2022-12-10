05b811909f764faa9839ec269f6b3b5e05108ba3
CristinaPicazo <derezade@gmail.com>
2022-12-10 10:21:05 +0100

    añadida suma por comando

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,16 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.numeroTextBox = new System.Windows.Forms.TextBox();
+            this.numeroUnoTextBox = new System.Windows.Forms.TextBox();
+            this.numeroDosTextBox = new System.Windows.Forms.TextBox();
+            this.resultadoLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(282, 46);
+            this.label1.Location = new System.Drawing.Point(279, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 24);
             this.label1.TabIndex = 0;
@@ -52,22 +54,42 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Aceptar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // numeroTextBox
+            // numeroUnoTextBox
             // 
-            this.numeroTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numeroTextBox.Location = new System.Drawing.Point(306, 91);
-            this.numeroTextBox.Name = "numeroTextBox";
-            this.numeroTextBox.Size = new System.Drawing.Size(100, 29);
-            this.numeroTextBox.TabIndex = 2;
+            this.numeroUnoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numeroUnoTextBox.Location = new System.Drawing.Point(227, 92);
+            this.numeroUnoTextBox.Name = "numeroUnoTextBox";
+            this.numeroUnoTextBox.Size = new System.Drawing.Size(100, 29);
+            this.numeroUnoTextBox.TabIndex = 2;
+            // 
+            // numeroDosTextBox
+            // 
+            this.numeroDosTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numeroDosTextBox.Location = new System.Drawing.Point(392, 92);
+            this.numeroDosTextBox.Name = "numeroDosTextBox";
+            this.numeroDosTextBox.Size = new System.Drawing.Size(100, 29);
+            this.numeroDosTextBox.TabIndex = 2;
+            // 
+            // resultadoLabel
+            // 
+            this.resultadoLabel.AutoSize = true;
+            this.resultadoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultadoLabel.Location = new System.Drawing.Point(279, 216);
+            this.resultadoLabel.Name = "resultadoLabel";
+            this.resultadoLabel.Size = new System.Drawing.Size(0, 24);
+            this.resultadoLabel.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.numeroTextBox);
+            this.Controls.Add(this.numeroDosTextBox);
+            this.Controls.Add(this.numeroUnoTextBox);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultadoLabel);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -80,7 +102,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox numeroTextBox;
+        private System.Windows.Forms.TextBox numeroUnoTextBox;
+        private System.Windows.Forms.TextBox numeroDosTextBox;
+        private System.Windows.Forms.Label resultadoLabel;
     }
 }
 
